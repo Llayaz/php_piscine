5853ac17cef4f57eca6e42852121280a1a975444
Llayaz <llaiyaz@gmail.com>
2022-06-07 20:50:17 +0300

    working photos

---
+++ day02/ex04/photos.php
@@ -0,0 +1,27 @@
+#!/usr/local/bin/php
+<?php
+	if ($argc == 2)
+	{
+		$url = $argv[1];
+		if (filter_var($url, FILTER_VALIDATE_URL))
+		{
+//			$folder = parse_url($url)['host'];
+			$dir = preg_replace('#^https?://#', '', $url);
+			$dir = preg_replace('/\/.*$/', '', $dir);
+			if (!file_exists($dir))
+				mkdir("$dir");
+			$page = file_get_contents($url);
+			preg_match_all('/<img.*?src=[\'"](.*?)[\'"].*?>/i', $page, $matches);
+			foreach ($matches[1] as $pic)
+			{
+				$path = $dir . "/" . basename($pic);
+				$file = fopen($path, 'w+');
+				$host = curl_init($pic);
+				curl_setopt($host, CURLOPT_FILE, $file);
+				curl_exec($host);
+				curl_close($host);
+				fclose($file);
+			}
+		}
+	}
+?>
