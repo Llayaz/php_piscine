41f68a3a4fa33f66fd0141cbd0b36b718a624b01
Llayaz <llaiyaz@gmail.com>
2022-06-08 10:35:58 +0300

    Update magnifying_glass.php

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -1,6 +1,5 @@
-#!/usr/local/bin/php
+#!/usr/bin/php
 <?php 
-
 function	toup($match)
 {
 	$res = strtoupper($match[0]);
@@ -10,11 +9,10 @@ function	toup($match)
 function	magnify($match)
 {
 	$str = $match[0];
-
-	//(?i) -> case insensitive, (?<= and ?= positive lookbehind and lookahead for exact pattern
-	$str = preg_replace_callback('/(?i)(?<=title=\")(.*)(?=\")/', 'toup', $str);
+	//(?i) -> case insensitive, (?<=  positive lookbehind for exact pattern, (.*?) as few times as possible
+	$str = preg_replace_callback('/(?mis)(?<=title=\")(.*?)(\")/', 'toup', $str);
 	//.*? -> match any character unlimited times as few times as possible
-	$str = preg_replace_callback('/>(.*?)</', 'toup', $str);
+	$str = preg_replace_callback('/(?mis)>(.*?)</', 'toup', $str);
 	return ($str);
 }
 
@@ -23,7 +21,7 @@ if ($argc == 2 && file_exists($argv[1]))
 	$page = file_get_contents($argv[1]);
 	if (!$page)
 		return ;
-	$page = preg_replace_callback('/(?i)(<a href=(.*))(.*)<\/a>/', 'magnify', $page);
+	$page = preg_replace_callback('/(?mis)(<a(.*))(.*)<\/a>/', 'magnify', $page);
 	echo $page;
 }
- ?>
+?>
