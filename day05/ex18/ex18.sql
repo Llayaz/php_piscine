5560deb3b8c2c16d7697c6a9d5265fb188bb5c4a
Llayaz <llaiyaz@gmail.com>
2022-06-14 12:55:55 +0300

    subject

--- day05/ex18/ex18.sql
+++ day05/ex18/ex18.sql
@@ -2,3 +2,8 @@ SELECT `name` FROM `distrib`
 WHERE `id_distrib` IN (42, 62, 63, 64, 65, 66, 67, 68, 69, 71, 88, 89, 90)
 OR LOWER(`name`) REGEXP '^[^y]*y[^y]*y[^y]*$'
 LIMIT 2, 5;
+
+SELECT `name` FROM `distrib`
+WHERE `id_distrib` IN (42, 62, 63, 64, 65, 66, 67, 68, 69, 71, 88, 89, 90)
+OR (CHAR_LENGTH(`name`) - CHAR_LENGTH(REPLACE(LOWER(`name`), "y", "")) = 2)
+LIMIT 2, 5;
