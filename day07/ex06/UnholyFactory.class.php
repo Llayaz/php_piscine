0a96e1c4e0ceefa0b7be3b688762e5a45fb47567
Llayaz <llaiyaz@gmail.com>
2022-06-15 16:17:24 +0300

    Update UnholyFactory.class.php

--- day07/ex06/UnholyFactory.class.php
+++ day07/ex06/UnholyFactory.class.php
@@ -1,4 +1,30 @@
 <?php 
+class UnholyFactory
+	{
+		private $_absorbed = array();
 
+		public function absorb ($input)
+		{
+			if ($input instanceof Fighter)
+			{
+				if (!array_key_exists($input->unit, $this->_absorbed))
+				{
+					$this->_absorbed[$input->unit] = $input;
+					echo "(Factory absorbed a fighter of type " . $input->unit . ")" . PHP_EOL;
+				} else {
+					echo "(Factory already absorbed a fighter of type " . $input->unit . ")" . PHP_EOL;
+				}
+					
+
+			} else {
+				echo "(Factory can't absorb this, it's not a fighter)" . PHP_EOL;
+			}
+		}
+
+		public function fabricate ()
+		{
+
+		}
+	}
 
  ?>
